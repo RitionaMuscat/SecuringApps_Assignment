9252f18eb157178bb4f8d0976a33612eccf3f82f
RitionaMuscat <ritiona.muscat.a100244@mcast.edu.mt>
2021-02-14 14:08:10 +0100

    further changes

--- SecuringApps.Application/SecuringApps.Presentation/Controllers/StudentWorkController.cs
+++ SecuringApps.Application/SecuringApps.Presentation/Controllers/StudentWorkController.cs
@@ -3,18 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SecuringApps.Application.Interfaces;
 using SecuringApps.Presentation.Models;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Mime;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace SecuringApps.Presentation.Controllers
 {
@@ -94,8 +90,7 @@ namespace SecuringApps.Presentation.Controllers
                         var val = from a in StudentTask
                                   where a.Id.Equals(data.StudentWork.StudentTask.Id)
                                   select a;
-                        //foreach (var item in value)
-                        //{
+
                         var StudentWork = _studentWorkService.GetStudentWork().ToList();
                         var vals = from v in StudentWork
                                    where v.StudentTask.Id.Equals(data.StudentWork.StudentTask.Id) && v.workOwner.Equals(_userManager.GetUserId(User))
@@ -109,24 +104,25 @@ namespace SecuringApps.Presentation.Controllers
                             }
                             if (vals.Any())
                             {
-
                                 ModelState.AddModelError(string.Empty, "Your work is already submitted");
                                 break;
-
                             }
-
                             else
                             {
                                 if (file.Length > 0)
                                 {
-                                    string newFilename = Guid.NewGuid() + System.IO.Path.GetExtension(file.FileName);
+                                    string newFilename = Guid.NewGuid() + Path.GetExtension(file.FileName);
 
                                     string absolutePath = _env.WebRootPath + @"\Files\";
 
+                                    string getFullFilePath = Path.GetFullPath(file.FileName);
                                     using (var stream = System.IO.File.Create(absolutePath + newFilename))
                                     {
                                         file.CopyTo(stream);
                                     }
+
+                                    CompareFileHashes(newFilename);
+
                                     data.StudentWork.filePath = @"\Files\" + newFilename; //relative Path
                                     data.StudentWork.workOwner = _userManager.GetUserId(User);
 
@@ -196,6 +192,67 @@ namespace SecuringApps.Presentation.Controllers
             return View();
 
         }
+        public bool FileEquals(string pathFile, string pathFile2)
+        {
+            var getAllFiles = _studentWorkService.GetStudentWork();
+            var getFileName = from a in getAllFiles
+                              select a;
+            foreach (var item in getAllFiles)
+            {
+                IFormFile file;
+                byte[] file1 = System.IO.File.ReadAllBytes(pathFile);
+                byte[] file2 = System.IO.File.ReadAllBytes(item.filePath.Substring(7));
+                if (file1.Length == file2.Length)
+                {
+                    for (int i = 0; i < file1.Length; i++)
+                    {
+                        if (file1[i] != file2[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void CompareFileHashes(string fileName1)
+        {
+
+            var getAllFiles = _studentWorkService.GetStudentWork();
+            var getFileName = from a in getAllFiles
+                              select a;
+
+            SHA256 sha = SHA256.Create("SHA256");
+            HashAlgorithm hash = HashAlgorithm.Create(sha.ToString());
+
+            byte[] fileHash1;
+            byte[] fileHash2;
+            fileName1 = _env.WebRootPath + @"\Files\" + fileName1;
+            var fileName2 = "";
+            if (getAllFiles.Any())
+            {
+
+                foreach (var item in getFileName)
+                {
+                    fileName2 = _env.WebRootPath + item.filePath;
+                    using (FileStream fileStream1 = new FileStream(fileName1, FileMode.Open),
+                      fileStream2 = new FileStream(fileName2, FileMode.Open))
+                    {
+                        // Compute file hashes
+                        fileHash1 = hash.ComputeHash(fileStream1);
+                        fileHash2 = hash.ComputeHash(fileStream2);
+                    }
+                    if (BitConverter.ToString(fileHash1) == BitConverter.ToString(fileHash2))
+                        ModelState.AddModelError(string.Empty, "File is the same");
+                }
+
+            }
+
+        }
+
     }
 }
 
